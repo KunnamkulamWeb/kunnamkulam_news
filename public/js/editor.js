a2f6f1d0c67f5c245beb9c3938fd1d3ef1a37588
KunnamkulamNews <web.kunnamkulamnews@gmail.com>
2024-02-22 11:19:44 +0530

    second commit

--- public/js/editor.js
+++ public/js/editor.js
@@ -194,8 +194,8 @@ function createHtml(newsTitle, newsBody, newsImage){
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://kunnamkulamnews.onrender.com/css/home.css">
-    <link rel="stylesheet" href="https://kunnamkulamnews.onrender.com/css/blog.css">
+    <link rel="stylesheet" href="/css/home.css">
+    <link rel="stylesheet" href="/css/blog.css">
     <script src="https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js"></script>
     <script src="https://www.gstatic.com/firebasejs/8.2.1/firebase-database.js"></script>
 </head>
@@ -410,7 +410,7 @@ function createHtml(newsTitle, newsBody, newsImage){
     
     <footer>
         <div style="display:flex;justify-content:center;flex-direction:column;width:100%">
-                <img src="img/kunnamkulam-news-white.png" class="footer-logo text-center" alt="">
+                <img src="/img/kunnamkulam-news-white.png" class="footer-logo text-center" alt="">
                 <div class="grey-line"></div>
                 </div>
                 <p>Made By Webweaver</p>
